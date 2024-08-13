e1a69f37c63954c4d7226721fb22b6f85b593c98
Collins nnaji <collinsnnaji1@gmail.com>
2024-08-13 16:14:20 +0100

    fdsffvgfs

--- src/api/gptService.js
+++ src/api/gptService.js
@@ -1,15 +1,21 @@
 // api/gptService.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_BACKEND_API_URL + '/api/analyze';
+const API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
 export const analyzeResume = async (data) => {
     try {
-        const response = await axios.post(API_URL, data, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+        const response = await axios.post(`${API_URL}/api/analyze`, data);
+        return response.data;
+    } catch (error) {
+        console.error('Error calling the backend:', error);
+        throw error;
+    }
+};
+
+export const rewriteCV = async (data) => {
+    try {
+        const response = await axios.post(`${API_URL}/api/rewrite-cv`, data);
         return response.data;
     } catch (error) {
         console.error('Error calling the backend:', error);
